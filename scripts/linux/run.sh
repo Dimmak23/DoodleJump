3d1fffbca7d9140b40aee35902a168f1b2b425a8
Dmytro Kovryzhenko <onemoremagistr@gmail.com>
2025-02-02 22:18:50 +0200

    optimized run script; documentation for run method

--- scripts/linux/run.sh
+++ scripts/linux/run.sh
@@ -1,30 +1,70 @@
 #!/bin/bash
 
-#? Use ./run.sh <config> to run any configured executable
+set -e  # Exit on error
 
-# ? Ensure a configuration parameter is provided
-if [[ -z "$1" ]]; then
-    echo "Usage: $0 <Debug|Release>"
-    echo "Invalid configuration: $config. Please specify 'Debug' or 'Release'."
+# TODO: Maybe passing configuration type will be used in the future 
+# #? Use ./run.sh <config> to run any configured executable
+# # ? Ensure a configuration parameter is provided
+# if [[ -z "$1" ]]; then
+#     echo "Usage: $0 <Debug|Release>"
+#     echo "Invalid configuration: $config. Please specify 'Debug' or 'Release'."
+#     exit 1
+# fi
+
+# config="$1"
+# TODO: Maybe passing configuration type will be used in the future 
+
+targetPath="game/source"
+exeFileName=""
+fullFilePath=""
+
+# ? Find the first .cpp file in the folder and extract its name
+for file in "$targetPath"/*.cpp; do
+    if [ -f "$file" ]; then
+        exeFileName="$(basename "$file" .cpp)"
+        break
+    fi
+done
+
+if [ -z "$exeFileName" ]; then
+    echo "No .cpp file found in the folder."
     exit 1
 fi
 
-config="$1"
+# ? Define the base folder where executables might be located
+BASE_FOLDER="$(pwd)/executables"
 
-targetPath="./game/source"
+# ? Check for Debug or Release folder
+if [ -d "$BASE_FOLDER/Release" ]; then
+    # ? Check if the executable exists
+    if [ -f "$BASE_FOLDER/Release/$exeFileName" ]; then
+        fullFilePath="$BASE_FOLDER/Release/$exeFileName"
+    fi
+fi
 
-# ? Get the first *.cpp file in the folder
-cppFile=$(find "$targetPath" -maxdepth 1 -type f -name "*.cpp" | head -n 1)
+if [ -d "$BASE_FOLDER/Debug" ]; then
+    # ? Check if the executable exists
+    if [ -f "$BASE_FOLDER/Debug/$exeFileName" ]; then
+        fullFilePath="$BASE_FOLDER/Debug/$exeFileName"
+    fi
+fi
 
-if [[ -n "$cppFile" ]]; then
-    cppFileName=$(basename "$cppFile")  # ? Extract the file name
-    exeFileName="${cppFileName%.cpp}"  # ? Replace .cpp extension with .exe
+if [ ! -f "$BASE_FOLDER/Release/$exeFileName" ]; then
+    if [ ! -f "$BASE_FOLDER/Debug/$exeFileName" ]; then
+        echo "No $BASE_FOLDER/Release/$exeFileName or $BASE_FOLDER/Debug/$exeFileName file have been found!"
+        exit 1
+    fi
+fi
 
+# "$fullFilePath" -window 1600x900
+"$fullFilePath" -window 1200x675
+# "$fullFilePath" -fullscreen 1600x900
+# "$fullFilePath" -fullscreen 2560x1440
+
+# TODO: Maybe passing configuration type will be used in the future 
     # Run the executable based on the specified configuration
-    "./executables/$config/$exeFileName" -window 1600x900
+    # "./executables/$config/$exeFileName" -window 1600x900
     # "./executables/$config/$exeFileName" -window 1200x675
     # "./executables/$config/$exeFileName" -fullscreen 1600x900
     # "./executables/$config/$exeFileName" -fullscreen 2560x1440
-else
-    echo "No .cpp file found in the folder."
-fi
+# TODO: Maybe passing configuration type will be used in the future 
--- scripts/win/run.bat
+++ scripts/win/run.bat
@@ -1,21 +1,24 @@
 @echo off
 setlocal enabledelayedexpansion
 
-@REM ? Ensure a configuration parameter is provided
-if "%~1"=="" (
-    echo Usage: %~nx0 ^<Debug^|Release^>
-    echo Invalid configuration: %config%. Please specify 'Debug' or 'Release'.
-    exit /b 1
-)
+@REM TODO: Maybe passing configuration type will be used in the future 
+@REM @REM ? Ensure a configuration parameter is provided
+@REM if "%~1"=="" (
+@REM     echo Usage: %~nx0 ^<Debug^|Release^>
+@REM     echo Invalid configuration: %config%. Please specify 'Debug' or 'Release'.
+@REM     exit /b 1
+@REM )
 
-set config=%1
+@REM set config=%1
+@REM TODO: Maybe passing configuration type will be used in the future 
 
-set targetPath=game\source
-set exeFileName=
+set "targetPath=game\source"
+set "exeFileName="
+set "fullFilePath="
 
 @REM ? Find the first .cpp file in the folder and extract its name
 for %%F in ("%targetPath%\*.cpp") do (
-    set exeFileName=%%~nF
+    set "exeFileName=%%~nF"
     goto :found
 )
 
@@ -25,10 +28,47 @@ if "%exeFileName%"=="" (
     exit /b 1
 )
 
-@REM ? Run the executable based on the specified configuration
-"executables\%config%\%exeFileName%.exe" -window 1600x900
-@REM "executables\%config%\%exeFileName%.exe" -window 1200x675
+@REM ? Define the base folder where executables might be located
+set "BASE_FOLDER=%CD%\executables"
+
+@REM ? Check for Release or Debug folder
+@REM ? Later for executable existance
+if exist "%BASE_FOLDER%\Release" (
+    echo "%BASE_FOLDER%\Release" folder found!
+    if exist "%BASE_FOLDER%\Release\%exeFileName%.exe" (
+        set "fullFilePath=%BASE_FOLDER%\Release\%exeFileName%.exe"
+    )
+)
+if exist "%BASE_FOLDER%\Debug" (
+    echo "%BASE_FOLDER%\Debug" folder found!
+    if exist "%BASE_FOLDER%\Debug\%exeFileName%.exe" (
+        set "fullFilePath=%BASE_FOLDER%\Debug\%exeFileName%.exe"
+    )
+)
+if not exist "%BASE_FOLDER%\Release\%exeFileName%.exe" (
+    if not exist "%BASE_FOLDER%\Debug\%exeFileName%.exe" (
+        echo No executables/Debug/%exeFileName%.exe or executables/Release/%exeFileName%.exe file found!
+        exit /b 1
+    )
+)
+
+if "%fullFilePath%"=="" (
+    echo Executable file not found.
+    exit /b 1
+)
+
+@REM ? Run the executable
+@REM "%fullFilePath%" -window 1600x900
+@REM "%fullFilePath%" -window 1200x675
+@REM "%fullFilePath%" -fullscreen 1600x900
+"%fullFilePath%" -fullscreen 2560x1440
+
+@REM TODO: Maybe passing configuration type will be used in the future 
+@REM @REM ? Run the executable based on the specified configuration
+@REM @REM "executables\%config%\%exeFileName%.exe" -window 1600x900
+@REM @REM "executables\%config%\%exeFileName%.exe" -window 1200x675
 @REM "executables\%config%\%exeFileName%.exe" -fullscreen 1600x900
-@REM "executables\%config%\%exeFileName%.exe" -fullscreen 2560x1440
+@REM @REM "executables\%config%\%exeFileName%.exe" -fullscreen 2560x1440
+@REM TODO: Maybe passing configuration type will be used in the future 
 
 endlocal
